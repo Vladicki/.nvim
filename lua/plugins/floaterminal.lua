c8d6d34d90bf8ed8aba2d9db62f205f46c7aefb6
Vladislav Iurev <vlad.yurev.06@gmail.com>
2025-08-05 18:49:07 +0400

    testing the telescope cm line

--- lua/plugins/floaterminal.lua
+++ lua/plugins/floaterminal.lua
@@ -57,7 +57,9 @@ return {
       end
 
       vim.api.nvim_create_user_command('Floaterminal', toggle_terminal, {})
-      vim.keymap.set('n', '<space><space>', toggle_terminal, { desc = 'Open Floating Terminal' })
+
+      -- TODO: TEMPERARY REMOVED IT TO TRY TELESCOPE CMDLINE
+      -- vim.keymap.set('n', '<space><space>', toggle_terminal, { desc = 'Open Floating Terminal' })
     end,
   },
 }
